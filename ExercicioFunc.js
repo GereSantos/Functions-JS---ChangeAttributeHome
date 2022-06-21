945e848bbb7a88cae03b2738581f7dc67b7794fe
Geremias Santos <Geremias99999@gmail.com>
2022-06-21 15:55:58 -0300

    Functions Change Color/Name Home

---
+++ ExercicioFunc.js
@@ -0,0 +1,20 @@
+//Programa que simula a troca de cor de uma casa
+//Utilizando Linguagem JS
+
+let Cor_da_casa = 'Vermelho'
+let Numero_Casa = 124
+function changecolor(){
+    return Cor_da_casa = 'Rosa';
+}
+
+console.log('Cor atual da casa : ' + Cor_da_casa)
+changecolor(Cor_da_casa)
+console.log('Nova cor da casa: ' + Cor_da_casa)
+
+function numberhome(){
+    return Numero_Casa = 180
+}
+console.log()
+console.log('Número atual da casa: ' + Numero_Casa)
+numberhome()
+console.log('Novo número da casa: ' + Numero_Casa)
